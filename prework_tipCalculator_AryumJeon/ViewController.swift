520f74d55ef6328c22536b954098161123e78b19
aryumjeon <zalwata@hotmail.com>
2021-01-05 17:19:40 -0800

    Initial Commit

---
+++ prework_tipCalculator_AryumJeon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  prework_tipCalculator_AryumJeon
+//
+//  Created by Aryum Jeon on 1/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ prework_tipCalculator_AryumJeonTests/prework_tipCalculator_AryumJeonTests.swift
@@ -0,0 +1,33 @@
+//
+//  prework_tipCalculator_AryumJeonTests.swift
+//  prework_tipCalculator_AryumJeonTests
+//
+//  Created by Aryum Jeon on 1/5/21.
+//
+
+import XCTest
+@testable import prework_tipCalculator_AryumJeon
+
+class prework_tipCalculator_AryumJeonTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
