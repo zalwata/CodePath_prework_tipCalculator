b02ab9b307d08a9280c634718eca72095b2e0ba8
aryumjeon <zalwata@hotmail.com>
2021-01-11 23:00:55 -0800

    onTap_keypad_disappear

--- prework_tipCalculator_AryumJeon/Controller/ViewController.swift
+++ prework_tipCalculator_AryumJeon/Controller/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onTapped(_ sender: Any) {
+        
+        view.endEditing(true)
+        
+    }
+    
 }
 
